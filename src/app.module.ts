a6d25732c9e2b276c4a57faf6f1fedfa6c719f61
Lucas Assis <lukas.ecomp@gmail.com>
2024-09-24 13:36:54 -0300

    connect to local database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://test:test@localhost:27017'),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

9533927f89a55234291e23a9c3e4c123cd380b8d
Lucas Assis <lukas.ecomp@gmail.com>
2024-10-01 08:44:08 -0300

    add api env vars to .env

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,13 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  api: {
+    baseUrl: process.env.API_BASE_URL,
+    port: parseInt(process.env.API_PORT, 10) || 3000,
+    enableDocs: process.env.API_ENABLE_DOCS === 'true',
+    nodeEnv: process.env.NODE_ENV,
+    bcrypt: {
+      saltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS),
+    },
+  },
   database: {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10) || 27017,
